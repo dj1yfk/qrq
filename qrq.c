27e1918bdfd7d3fd739dc02bb8fea51dbc2f9a6e
Fabian Kurz <fabian@fkurz.net>
2008-06-22 14:08:40 +0000

    Added $Id$s, AUTHORS file.

--- qrq.c
+++ qrq.c
@@ -2,6 +2,8 @@
 qrq - High speed morse trainer, similar to DL4MM's Rufz    
 Copyright (C) 2006-2007  Fabian Kurz
 
+$Id$
+
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
 Foundation; either version 2 of the License, or (at your option) any later
